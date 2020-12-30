4802b23f132e94aa96733d42a38732fdf72c247d
puspa <blackie002@gmail.com>
2020-12-30 09:09:07 -0500

    graphql schema created

---
+++ schema/schema.js
@@ -0,0 +1,14 @@
+const graphql = require('graphql');
+const axios =  require(axios);
+
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLList,
+    GraphQLSchema,
+    GraphQLNonNull,
+    GraphQLID,
+    ObjectID,
+  } = graphql;
