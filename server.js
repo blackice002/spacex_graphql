5d909ff5c69a6cdf1cfadc5adf763c62749b7031
puspa <blackie002@gmail.com>
2020-12-30 09:19:17 -0500

    expres- graphql added to the server

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const app =express();
-const env = require('dotenv');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const app = express();
+const env = require("dotenv");
+const schema = require("./schema/schema");
+const cors = require('cors')
 
 env.config();
 
+app.use(cors())
+app.use(
+        "/graphql",
+        graphqlHTTP({
+            schema,
+            graphiql: true,
+        })
+        );
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>console.log(`App running on port : ${PORT}`))
-
-
-
+app.listen(PORT, () => console.log(`App running on port : ${PORT}`));
