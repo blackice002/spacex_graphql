fc4134286c95c69a510567e4d39351976f609622
puspa <blackie002@gmail.com>
2021-01-02 17:06:18 -0500

    heroku test 1/2

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ const { graphqlHTTP } = require("express-graphql");
 const app = express();
 const env = require("dotenv");
 const schema = require("./schema/schema");
-const cors = require('cors')
+const cors = require("cors");
+const path = require('path')
 
 env.config();
 
-app.use(cors()) 
+app.use(cors());
 app.use(
-        "/graphql",
-        graphqlHTTP({
-            schema,
-            graphiql: true,
-        })
-        );
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
