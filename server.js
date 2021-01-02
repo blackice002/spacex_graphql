fb803269593b98af3b5c6ae0e46b2c29f245fa2f
puspa <blackie002@gmail.com>
2021-01-02 18:12:11 -0500

    heroku check -1

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ app.use(
 );
 
 app.use(express.static('public'));
+
 app.get("*", (req,res)=> res.sendFile(path.resolve(__dirname, "public", "index.html")))
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`App running on port : ${PORT}`));
